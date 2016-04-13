35a4fbd4d98c645c018991095f5b00db1222b398
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-04-13 21:42:09 +1000

    Adding the file creation

--- src/MySQL2Graph.java
+++ src/MySQL2Graph.java
@@ -1,9 +1,21 @@
+import java.io.FileWriter;
 
 public class MySQL2Graph {
 
+	public static String url = "jdbc:mysql://localhost:8889/wine";
+	public static String username = "root";
+	public static String password = "1234";
+	
 	public static void main(String[] args) {
-		System.out.println("Hi");
-
+		
+		FileWriter fw;
+		try {
+			fw = new FileWriter("graph.net");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 
 }
